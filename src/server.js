eeb4a99f24c60226e89daf699c39edfc0585d2b5
ms3221 <hyeongjun3221@gmail.com>
2022-11-03 15:16:01 +0900

    df

--- src/server.js
+++ src/server.js
@@ -10,8 +10,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => {
-  console.log(1);
-  res.render("home");
+  res.send("안녕하세요!")
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
